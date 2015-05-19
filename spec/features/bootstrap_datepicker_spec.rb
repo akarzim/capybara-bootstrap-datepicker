768ce4810602a62e74fed3425511d4bd7062ad7b
François Vantomme <akarzim@gmail.com>
2015-05-19 20:07:37 +0200

    tests(input): add a test filling an input without using the datepicker

--- spec/features/bootstrap_datepicker_spec.rb
+++ spec/features/bootstrap_datepicker_spec.rb
@@ -8,12 +8,17 @@ RSpec.describe 'Bootstrap Datepicker', type: :feature do
     expect(page).to have_content 'Bootstrap3 Datepicker'
   end
 
-  it 'fills in datepicker based on Bootstrap 3.3', js: true do
+  it 'fills in an input without using the datepicker', js: true do
+    select_date Date.today, from: 'Label of my date input'
+    expect(Date.parse find_field('Label of my date input').value).to eq Date.today
+  end
+
+  it 'fills in a datepicker based on Bootstrap 3.3', js: true do
     select_date Date.today, from: 'Label of my date input', datepicker: :bootstrap
     expect(Date.parse find_field('Label of my date input').value).to eq Date.today
   end
 
-  it 'fills in localized datepicker based on Bootstrap 3.3', js: true do
+  it 'fills in a localized datepicker based on Bootstrap 3.3', js: true do
     select_date Date.today, from: 'Label of my localized date input', datepicker: :bootstrap
     expect(Date.parse find_field('Label of my localized date input').value).to eq Date.today
   end
