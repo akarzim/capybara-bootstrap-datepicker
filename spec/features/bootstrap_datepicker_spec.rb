675da6326cc7132c65aba3a1b6b4e876349678b6
François Vantomme <akarzim@pm.me>
2025-03-30 20:09:13 +0200

    style(rubocop): autocorrect RSpec cops

--- spec/features/bootstrap_datepicker_spec.rb
+++ spec/features/bootstrap_datepicker_spec.rb
@@ -4,22 +4,22 @@ RSpec.shared_examples 'a datepicker' do
   context 'default date' do
     subject { Date.parse(find_field('Label of my date input').value) }
 
-    it 'fills in an input without using the datepicker', js: true do
+    it 'fills in an input without using the datepicker', :js do
       select_date Date.today, from: 'Label of my date input'
       expect(subject).to eq Date.today
     end
 
-    it 'fills in a standard datepicker', js: true do
+    it 'fills in a standard datepicker', :js do
       select_date Date.today, from: 'Label of my date input', datepicker: :simple
       expect(subject).to eq Date.today
     end
 
-    it 'fills in a datepicker based on Bootstrap', js: true do
+    it 'fills in a datepicker based on Bootstrap', :js do
       select_date Date.today, from: 'Label of my date input', datepicker: :bootstrap
       expect(subject).to eq Date.today
     end
 
-    it 'fills in an input with DateTime object', js: true do
+    it 'fills in an input with DateTime object', :js do
       select_date DateTime.now, from: 'Label of my date input'
       expect(subject).to eq Date.today
     end
@@ -28,7 +28,7 @@ RSpec.shared_examples 'a datepicker' do
   context 'locale date' do
     subject { Date.parse(find_field('Label of my localized date input').value) }
 
-    it 'fills in a localized datepicker based on Bootstrap', js: true do
+    it 'fills in a localized datepicker based on Bootstrap', :js do
       select_date Date.today, from: 'Label of my localized date input', datepicker: :bootstrap
       expect(subject).to eq Date.today
     end
@@ -37,7 +37,7 @@ RSpec.shared_examples 'a datepicker' do
   context 'required date' do
     subject { Date.parse(find_field('Start date').value) }
 
-    it 'fills in a required datepicker based on Bootstrap', js: true do
+    it 'fills in a required datepicker based on Bootstrap', :js do
       select_date Date.today, from: 'Start date', datepicker: :bootstrap
       expect(subject).to eq Date.today
     end
@@ -46,11 +46,11 @@ end
 
 RSpec.describe 'Bootstrap Datepicker', type: :feature do
   describe 'Boostrap 3.4' do
-    before :each do
+    before do
       Capybara.current_session.driver.visit "#{Capybara.app_host}/bootstrap-3.4.html"
     end
 
-    it 'loads the page correctly', js: true do
+    it 'loads the page correctly', :js do
       expect(page).to have_content 'Bootstrap 3.4 Datepicker'
     end
 
@@ -58,11 +58,11 @@ RSpec.describe 'Bootstrap Datepicker', type: :feature do
   end
 
   describe 'Boostrap 4.4' do
-    before :each do
+    before do
       Capybara.current_session.driver.visit "#{Capybara.app_host}/bootstrap-4.4.html"
     end
 
-    it 'loads the page correctly', js: true do
+    it 'loads the page correctly', :js do
       expect(page).to have_content 'Bootstrap 4.4 Datepicker'
     end
 
@@ -70,11 +70,11 @@ RSpec.describe 'Bootstrap Datepicker', type: :feature do
   end
 
   describe 'Boostrap 5.0' do
-    before :each do
+    before do
       Capybara.current_session.driver.visit "#{Capybara.app_host}/bootstrap-5.0.html"
     end
 
-    it 'loads the page correctly', js: true do
+    it 'loads the page correctly', :js do
       expect(page).to have_content 'Bootstrap 5.0 Datepicker'
     end
 
